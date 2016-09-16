b5969d7e99c822133520d3c6142fb7821ba37ddf
Loomess <ice-c@hotmail.de>
2016-09-16 11:55:42 +0200

    commit Michael LottoKlassenDiagramm

--- Lotto/Lotto/Lottoschein.cs
+++ Lotto/Lotto/Lottoschein.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace Lotto
 {
     public class Lottoschein
     {
+        private ArrayList tipps;
+        private int superzahl;
+
+        public int Superzahl
+        {
+            get { return superzahl; }
+            set { superzahl = value; }
+        }
+
+        public void AddTipp(int [] tipp)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateTipp(int tippnummer, int[] tipp)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
