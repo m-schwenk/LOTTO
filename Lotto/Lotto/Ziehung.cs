d65a2ab6b6a17a224186dcc02db4ed3a6604d97b
Maya Schwenk <mschwenk.wbs@gmail.com>
2016-09-28 16:41:51 +0200

    Struktur fuer Ziehungen

---
+++ Lotto/Lotto/Ziehung.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lotto
+{
+    public struct Ziehung
+    {
+        public SortedSet<int> ZiehungsZahlen { get; private set; }
+        public int Superzahl { get; private set; }
+        public DateTime ZiehungsTag { get; set; }
+        public string Spiel77 { get; set; }
+        public string Super6 { get; set; }
+
+        public Ziehung(int[] ziehungsZahlen, int superZahl, DateTime ziehungsTag, string spiel77 = "", string super6 = "") : this()
+        {
+            ZiehungsZahlen = new SortedSet<int>(ziehungsZahlen);
+            if (ZiehungsZahlen.Count != 6)
+            {
+                throw new ArgumentException("Ungueltige Ziehungszahlen");
+            }
+            Superzahl = superZahl;
+            ZiehungsTag = ziehungsTag;
+            Spiel77 = spiel77;
+            Super6 = super6;
+        }
+
+        public Ziehung(int[] ziehungsZahlen, int superZahl) : this(ziehungsZahlen, superZahl, DateTime.Today) {}
+
+    }
+}
